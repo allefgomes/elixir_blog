95db2b14f1da0eed9c914a68d98b9f7463f72d2a
Allef Gomes <allefgalmeida@gmail.com>
2021-06-19 16:28:15 -0300

    create post migration and schema and refac controller

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -1,19 +1,17 @@
 defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
+  alias Blog.Repo
+  alias Blog.Posts.Post
+
   def index(conn, _params) do
-    posts = [
-      %{id: 1, title: "Post 1", url: "#"},
-      %{id: 2, title: "Post 2", url: "#"},
-      %{id: 3, title: "Post 3", url: "#"},
-      %{id: 4, title: "Post 4", url: "#"}
-    ]
+    posts = Repo.all(Post)
 
     render(conn, "index.html", posts: posts)
   end
 
   def show(conn, %{"id" => id}) do
-    post = %{id: id, title: "Post 1", url: "#"}
+    post = Repo.get(Post, id)
 
     render(conn, "show.html", post: post)
   end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule Blog.DatabaseSeeder do
+  alias Blog.Repo
+  alias Blog.Posts.Post
+
+  def insert_post(number) do
+    post = Post.changeset(
+      %Post{},
+      %{
+        title: "Post #{number}",
+        description: "#{number} - It is a long established fact that a reader will be distracted by the readable content of a(injected humour and the like)."
+      }
+    )
+
+    Repo.insert!(post)
+  end
+end
+
+(1..10) |> Enum.each(fn number -> Blog.DatabaseSeeder.insert_post(number) end)
